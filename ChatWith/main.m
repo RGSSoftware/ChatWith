dcbc714ed8311c397821d8bee89f449d8b7d04c7
Randel Smith <homankids@gmail.com>
2014-08-23 09:21:21 -0500

    Initial commit

---
+++ ChatWith/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ChatWith
+//
+//  Created by PC on 8/23/14.
+//  Copyright (c) 2014 Randel Smith. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RGSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([RGSAppDelegate class]));
+    }
+}
