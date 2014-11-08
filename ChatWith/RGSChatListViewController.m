0f3dee6e524cea30933629013736ab0ec0bcf4f4
Randel Smith <homankids@gmail.com>
2014-11-07 18:42:05 -0600

    Added Screen title to RGSChatListViewController

--- ChatWith/RGSChatListViewController.m
+++ ChatWith/RGSChatListViewController.m
@@ -61,6 +61,8 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     
+    self.navigationItem.title = @"Chats";
+    
     // Uncomment the following line to preserve selection between presentations.
     // self.clearsSelectionOnViewWillAppear = NO;
     
