5fc9dd96af1cf34064bfd786c73aea14e24cdbdb
Randel Smith <homankids@gmail.com>
2014-11-24 19:13:55 -0600

    Added RGSMessageComposerImage

---
+++ ChatWith/RGSMessageComposerImage.h
@@ -0,0 +1,16 @@
+//
+//  RGSMessageComposerImage.h
+//  ChatWith
+//
+//  Created by PC on 11/24/14.
+//  Copyright (c) 2014 Randel Smith. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface RGSMessageComposerImage : NSObject
+
+@property (strong, nonatomic)UIImage *image;
+@property NSInteger location;
+@property NSInteger index;
+@end
---
+++ ChatWith/RGSMessageComposerImage.m
@@ -0,0 +1,13 @@
+//
+//  RGSMessageComposerImage.m
+//  ChatWith
+//
+//  Created by PC on 11/24/14.
+//  Copyright (c) 2014 Randel Smith. All rights reserved.
+//
+
+#import "RGSMessageComposerImage.h"
+
+@implementation RGSMessageComposerImage
+
+@end
