eb927a6da861ae64ac699e61b7f1b1201902e9e7
Randel Smith <homankids@gmail.com>
2015-01-05 09:44:37 -0600

    Added Contact detail button to RGSContactDetailHandler

--- ChatWith/RGSContactDetailHandler.m
+++ ChatWith/RGSContactDetailHandler.m
@@ -11,24 +11,37 @@
 #import "RGSContact.h"
 #import "RGSManagedUser.h"
 
+#import "UIImage+Resize.h"
+
+const float scaleUpAnimateDuration = .8;
+
+@interface RGSContactDetailHandler ()
+@property UIButton *chatContactButton;
+@property UIButton *deleteContactButton;
+@end
+
 
 @implementation RGSContactDetailHandler
+- (void)animateDetailButtons {
+    [UIView animateWithDuration:(scaleUpAnimateDuration -.3) delay:.2 options:UIViewAnimationOptionCurveLinear animations:^{
+        self.chatContactButton.alpha = 1;
+    } completion:nil];
+    [UIView animateWithDuration:(scaleUpAnimateDuration -.5) delay:.4 options:UIViewAnimationOptionCurveLinear animations:^{
+        self.deleteContactButton.alpha = 1;
+    } completion:nil];
+}
+
 -(void)contactListViewController:(RGSContactListViewController *)contactListViewController didSelectContactAtIndex:(NSIndexPath *)contactIndex{
     RGSContactCell *contactCell = [contactListViewController contactCellAtIndex:contactIndex];
     
-    NSLog(@"contactCell.thumbnailImageView.frame:%@", NSStringFromCGRect([contactCell.contentView convertRect:contactCell.thumbnailImageView.frame toView:contactListViewController.view]));
-    
-    NSLog(@"screen rect:%@", NSStringFromCGRect([UIScreen mainScreen].bounds));
-    
-
     CGRect thumbnailImageRect = [contactCell.contentView convertRect:contactCell.thumbnailImageView.frame toView:contactListViewController.view];
+    NSLog(@"rect:%@", NSStringFromCGRect(thumbnailImageRect));
     
     float scalingFactor = 1.6;
     
     
     CGAffineTransform t = CGAffineTransformMakeScale(scalingFactor, scalingFactor);
     CGRect scaledRect = [self scaleRect:thumbnailImageRect withTrasformation:t];
-    
     
     UIImageView *thumbnailImageView = [[UIImageView alloc] initWithFrame:thumbnailImageRect];
     thumbnailImageView.image = contactCell.thumbnailImageView.image;
@@ -39,12 +52,34 @@
     
     
     
+    
+    
+    
+    self.chatContactButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.chatContactButton.frame = CGRectMake(CGRectGetMaxX(scaledRect) + 5, CGRectGetMinY(scaledRect) + 5, 40, 40);
+    self.chatContactButton.backgroundColor = [UIColor colorWithWhite:0.141 alpha:1.000];
+    [self.chatContactButton setImage:[[UIImage imageNamed:@"chat_Contact"] resizedImage:CGSizeMake(20, 20)] forState:UIControlStateNormal];
+    self.chatContactButton.alpha = 0;
+    self.chatContactButton.layer.cornerRadius = 10;
+    
+    self.deleteContactButton = [UIButton buttonWithType:UIButtonTypeCustom];
+    self.deleteContactButton.frame = CGRectMake(CGRectGetMaxX(scaledRect) + 5, CGRectGetMaxY(self.chatContactButton.frame) + 5, 40, 40);
+    self.deleteContactButton.backgroundColor = [UIColor colorWithWhite:0.141 alpha:1.000];
+    [self.deleteContactButton setImage:[[UIImage imageNamed:@"delete_Contact"] resizedImage:CGSizeMake(12, 12)] forState:UIControlStateNormal];
+    self.deleteContactButton.alpha = 0;
+    self.deleteContactButton.layer.cornerRadius = 10;
+    
+    [contactListViewController.view addSubview:self.deleteContactButton];
+    [contactListViewController.view addSubview:self.chatContactButton];
+
     if(CGRectContainsRect([UIScreen mainScreen].bounds, scaledRect)){
         NSLog(@"scaled Rect is inside mainScreen");
         
-        [UIView animateWithDuration:1 animations:^{
+        [UIView animateWithDuration:scaleUpAnimateDuration animations:^{
             thumbnailImageView.transform = t;
         }];
+        
+        [self animateDetailButtons];
         
 
     } else {
@@ -52,40 +87,45 @@
         int padding = 8;
         
         CGAffineTransform translation;
-        
-        UIView *pupleView = [UIView new];
-        pupleView.backgroundColor = [UIColor purpleColor];
-        CGRect pupleViewFrame = CGRectZero;
-        pupleViewFrame.size = CGSizeMake(20, scaledRect.size.height);
-        pupleViewFrame.origin.y = CGRectGetMinY(scaledRect) + padding;
-        pupleView.alpha = 0;
-        
-        [contactListViewController.view addSubview:pupleView];
+
+        CGRect chatButtonFrame = self.chatContactButton.frame;
+        CGRect deleteButtonFrame = self.deleteContactButton.frame;
         
         if(CGRectGetMinX(scaledRect) < 0.0f){
             float rightShift = padding -(CGRectGetMinX(scaledRect));
             
             translation = CGAffineTransformMakeTranslation(rightShift, padding);
             
-            pupleViewFrame.origin.x = CGRectGetMaxX(scaledRect) + 10 + padding;
+            
+            chatButtonFrame.origin.x += rightShift;
+            chatButtonFrame.origin.y += padding;
+            self.chatContactButton.frame = chatButtonFrame;
+            
+            deleteButtonFrame.origin.x += rightShift;
+            deleteButtonFrame.origin.y += padding;
+            self.deleteContactButton.frame = deleteButtonFrame;
             
         } else {
             float leftShift = -(CGRectGetMaxX(scaledRect) - CGRectGetWidth([UIScreen mainScreen].bounds) + padding);
             
             translation = CGAffineTransformMakeTranslation(leftShift, padding);
             
-            pupleViewFrame.origin.x = (CGRectGetMinX(scaledRect)+leftShift) - 28;
+            chatButtonFrame.origin.x = (CGRectGetMinX(scaledRect)+leftShift) - (padding + CGRectGetWidth(self.chatContactButton.frame));
+            chatButtonFrame.origin.y = CGRectGetMinY(scaledRect) + 5 + padding;
+            self.chatContactButton.frame = chatButtonFrame;
+            
+            
+            deleteButtonFrame.origin.x = chatButtonFrame.origin.x;
+            deleteButtonFrame.origin.y = CGRectGetMaxY(chatButtonFrame) + 5;
+            self.deleteContactButton.frame = deleteButtonFrame;
         }
         
-        pupleView.frame = pupleViewFrame;
-        
-        
-        
         CGAffineTransform translationWithScaling = CGAffineTransformScale(translation, scalingFactor, scalingFactor);
-        [UIView animateWithDuration:.8 animations:^{
+        [UIView animateWithDuration:scaleUpAnimateDuration animations:^{
             thumbnailImageView.transform = translationWithScaling;
-            pupleView.alpha = 1;
         }];
+        
+        [self animateDetailButtons];
     }
 }
 
